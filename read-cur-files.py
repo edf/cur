6c979a85f4816ddbf4547a6281203b52af4bfda9
Ed <edf@users.noreply.github.com>
2025-04-03 23:10:03 -0600

    Update read-cur-files.py
    working code

--- read-cur-files.py
+++ read-cur-files.py
@@ -1,27 +1,41 @@
 import os
 import pandas as pd
 import argparse
+from datetime import datetime
+from tabulate import tabulate
 
 def process_cur_files(directory):
-    # List all files in the directory
-    files = [filename for filename in os.listdir(directory) if os.path.isfile(os.path.join(directory, filename))]
-    
-    for file in files:
-        file_path = os.path.join(directory, file)
-        # Read the CUR file into a DataFrame
-        df = pd.read_csv(file_path)
-        # Transpose the DataFrame
-        df_transposed = df.transpose()
-        # Print the column headers
-        print(f"Column headers for {file}: {list(df_transposed.columns)}")
-        # Number the rows
-        df_transposed.index = range(1, len(df_transposed) + 1)
-        # Print the DataFrame with numbered rows
-        print(f"Transposed DataFrame for {file}:\n{df_transposed}\n")
+
+    dataframes = []
+
+    for month_dir in os.listdir(directory):
+        month_dir_path = os.path.join(directory, month_dir)
+        if os.path.isdir(month_dir_path):
+            for filename in os.listdir(month_dir_path):
+                if filename.endswith('.parquet'):
+                    file_path = os.path.join(month_dir_path, filename)
+                    df = pd.read_parquet(file_path)
+                    dataframes.append(df)
+    combined_df = pd.concat(dataframes, ignore_index=True)
+    transposed_df = combined_df.head().transpose()
+    transposed_df.insert(0, 'Row Number', range(1, 1 + len(transposed_df)))
+
+    return transposed_df
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process CUR files.')
     parser.add_argument('-d', '--directory', type=str, default='sample/year=2018', help='Directory containing the CUR files')
     args = parser.parse_args()
-    
-    process_cur_files(args.directory)
+    print(args)
+
+    cur_content = process_cur_files(args.directory)
+    print(f"variable type is: " + str(type(cur_content)))
+
+    timestamp = datetime.now().strftime('%Y-%m%d-%H%M')
+    output_filename = f'cur-sample-{timestamp}.txt'
+
+    with open(output_filename, 'w') as f:
+        headers = [f"{i}  {col}" for i, col in enumerate(cur_content.columns, start=1)]
+        f.write(tabulate(cur_content, headers=headers, tablefmt='psql'))
+    print(f"data written to {output_filename}")
